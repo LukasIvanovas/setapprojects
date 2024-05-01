ed62a1fc5c6ba28ad56f405bb190aa5faa97c092
fnewton3 <128034196+fnewton3@users.noreply.github.com>
2024-05-01 09:35:25 +0100

    file names/ create event
    more useful/coherent file names. and made changes to create event, it has date selection.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// -- THIS FILE SHOULD ONLY HAVE RUNNING MATERIAL/NAVIAGATION- SIMILIAR TO THE FOLLOWING
+
+// import 'package:flutter/material.dart';
+// import 'signup.dart';
+// import 'login.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Login(),
+//     );
+//   }
+// }
