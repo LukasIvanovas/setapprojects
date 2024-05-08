6d9518c53d5f8e0b103d93a4116f337a12ccf7a1
Skipper1404 <maxhartwell1404@icloud.com>
2024-05-08 08:36:22 +0100

    Ttest

---
+++ setapprojects-firebase-test/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test"
+  "FLUTTER_TARGET=M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test\\lib\\pages\\login.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80YTU4NWI3OTI5NGU4MzBmYTg5YzI0OTI0ZDU4YTI3Y2M4ZmJmNDA2Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=M:\\Other\\CursorCode\\setapprojects-firebase-test\\setapprojects-firebase-test\\.dart_tool\\package_config.json"
+)
