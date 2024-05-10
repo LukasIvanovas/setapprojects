1e1e31f590274c79adf424d1a79186c4f3703717
up2157231 <66753907+up2157231@users.noreply.github.com>
2024-05-10 18:45:23 +0100

    Add files via upload

---
+++ lib/pages/forgotPassword.dart
@@ -0,0 +1,399 @@
+import 'package:flutter/material.dart';
+import 'user_model.dart';
+import 'package:get/get.dart';
+import 'user_repository.dart';
+import 'login.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'profile_controller.dart';
+
+class ForgotPassword extends StatefulWidget {
+  @override
+  _PasswordState createState() => _PasswordState();
+}
+
+class _PasswordState extends State<ForgotPassword> {
+
+  final emailGiven = TextEditingController();
+  final usernameGiven = TextEditingController();
+  final questions1 = TextEditingController();
+  final questions2 = TextEditingController();
+  final newPassword = TextEditingController();
+  final controller = Get.put(ProfileController());
+
+  bool _passwordVisible = false; // Track password visibility
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(
+                "https://cdn3.iconfinder.com/data/icons/network-and-communications-6/130/291-128.png",
+                height: 90,
+                width: 90,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Planify",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  color: Color(0xff3a57e8),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Forgot Password",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                child: TextField(
+                  controller: emailGiven,
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    labelText: "Email",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff9e9e9e),
+                    ),
+                    filled: true,
+                    fillColor: Color(0x00f2f2f3),
+                    isDense: false,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                child: TextField(
+                  controller: usernameGiven,
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    labelText: "Name",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff9e9e9e),
+                    ),
+                    filled: true,
+                    fillColor: Color(0x00f2f2f3),
+                    isDense: false,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                child: TextField(
+                  controller: questions1,
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    labelText: "Security Question 1: What was your childhood home street address?",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff9e9e9e),
+                    ),
+                    filled: true,
+                    fillColor: Color(0x00f2f2f3),
+                    isDense: false,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                child: TextField(
+                  controller: questions2,
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    labelText: "Security Question 2: What is your mother's maiden name?",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff9e9e9e),
+                    ),
+                    filled: true,
+                    fillColor: Color(0x00f2f2f3),
+                    isDense: false,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                child: TextField(
+                  controller: newPassword,
+                  obscureText: !_passwordVisible,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(color: Color(0xff9e9e9e), width: 1),
+                    ),
+                    labelText: "New Password",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff9e9e9e),
+                    ),
+                    filled: true,
+                    fillColor: Color(0x00f2f2f3),
+                    isDense: false,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    suffixIcon: IconButton( // Add suffix icon for password visibility toggle
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible; // Toggle visibility
+                        });
+                      },
+                      icon: Icon(
+                        _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: Color(0xff9e9e9e),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 16),
+                child: MaterialButton(
+                  onPressed: () async {
+                    List<UserModel> userDataList = await ProfileController.instance.getUserData(emailGiven.text);
+                    if (_validatePassword(newPassword.text)) {
+                      if (userDataList[0].userName == usernameGiven.text && userDataList[0].question1 == questions1.text && userDataList[0].question2 == questions2.text) {
+                        await ProfileController.instance.updatePassword(userDataList[0], newPassword.text);
+                        _showSuccessDialog(context);
+                      } else {
+                        _showIncorrectInfoDialog(context);
+                      }
+                    } else {
+                      // Show password requirements error message
+                      _showPasswordRequirementsDialog(context);
+                    }
+                  },
+                  color: Color(0xff3a57e8),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    "Reset Password",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  textColor: Color(0xffffffff),
+                  height: 40,
+                  minWidth: 140,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool _validatePassword(String password) {
+    if (password.length < 8) return false;
+    if (!password.contains(RegExp(r'[0-9]'))) return false;
+    if (!password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) return false;
+    return true;
+  }
+
+  void _showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Success"),
+          content: Text("Password changed successfully!"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Navigate to the Login screen
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Login(),
+                  ),
+                );
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Show password requirements error message
+  void _showPasswordRequirementsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Password Requirements"),
+          content: Text(
+              "Password must be at least 8 characters long and contain at least one number and one symbol."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showIncorrectInfoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Incorrect Information"),
+          content: Text(
+              "The information given is incorrect."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
